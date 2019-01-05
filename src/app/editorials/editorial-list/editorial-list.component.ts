fc728fba0c1d84b88f2e354923ca1ce7ac858313
Project Illumine <project-illumine@gmail.com>
2019-01-05 17:40:37 -0500

    Implemented band-aid to prevent disclaimer from overlapping editorials

--- src/app/editorials/editorial-list/editorial-list.component.ts
+++ src/app/editorials/editorial-list/editorial-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Editorial } from 'app/domain/editorial';
 import { EditorialService } from 'app/services/editorial.service';
 import { OrganizationService } from 'app/services/organization.service';
+import { Router } from '@angular/router';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
 
 @Component({
   selector: 'app-editorial-list',
@@ -14,15 +16,24 @@ export class EditorialListComponent implements OnInit {
   editorials: Editorial[];
 
   constructor(private editorialService: EditorialService,
-    private organizationService: OrganizationService) { }
+    private router: Router) { }
 
   ngOnInit() {
     this.getEditorials();
+    this.removeDisclaimer();
   }
 
   getEditorials(): void {
     this.editorialService.geteditorials()
       .subscribe(retrievedEditorials => this.editorials = retrievedEditorials);
+  }
+
+  removeDisclaimer(): void {
+    let disclaimer = document.getElementById("disclaimerMsg");
+    console.log(this.router.url);
+    if (disclaimer && this.router.url != "/") {
+      disclaimer.remove();
+    }
   }
 }
 
