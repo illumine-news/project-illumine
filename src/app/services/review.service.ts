3ccabf795af855beb79c18adb4038686782d116a
Project Illumine <illumine.news@gmail.com>
2019-01-12 14:45:04 -0500

    removed more commented code

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 @Injectable({
@@ -18,69 +18,36 @@ export class ReviewService {
 
   constructor(
     private http: HttpClient,
-    ) { }
+  ) { }
 
-private reviewsUrl = 'api/reviews/';
+  private reviewsUrl = 'api/reviews/';
 
-getreviewsforarticle(articleId: any): Observable<Review[]> {
-  if (!articleId)
-  {
-    console.log("Article ID not found");
-    return of ([]);
-  }
-  return this.http.get<Review[]>(`${this.reviewsUrl}/?articleId=${articleId}`).pipe(
-  catchError(this.handleError<Review[]>('getReviewsForArticle', []))
-  );
-}  
-
-getreviews(): Observable<Review[]> {
-   return this.http.get<Review[]>(this.reviewsUrl)
-   .pipe(
-    catchError(this.handleError('getreviews', []))
-   );
-  }
-
-  addreview (review: Review): Observable<Review> {
-    return this.http.post<Review>(this.reviewsUrl, review, httpOptions).pipe(
-      catchError(this.handleError<Review>('addreview'))
+  getreviewsforarticle(articleId: any): Observable<Review[]> {
+    if (!articleId) {
+      console.log("Article ID not found");
+      return of([]);
+    }
+    return this.http.get<Review[]>(`${this.reviewsUrl}/?articleId=${articleId}`).pipe(
+      catchError(this.handleError<Review[]>('getReviewsForArticle', []))
     );
   }
 
-  updatereview (review: Review): Observable<any> {
-    return this.http.put(this.reviewsUrl, review, httpOptions).pipe(
-      catchError(this.handleError<any>('updatereview'))
-    );
+  getreviews(): Observable<Review[]> {
+    return this.http.get<Review[]>(this.reviewsUrl)
+      .pipe(
+        catchError(this.handleError('getreviews', []))
+      );
   }
 
-  deletereview (review: Review | number): Observable<Review> {
-    const id = typeof review === 'number' ? review : review.id;
-    const url = `${this.reviewsUrl}/${id}`;
-
-    return this.http.delete<Review>(url, httpOptions).pipe(
-      catchError(this.handleError<Review>('deletereview'))
-    );
-  }
-
-  /* GET reviews whose name contains search term */
-searchreviews(term: string): Observable<Review[]> {
-  if (!term.trim()) {
-    // if not search term, return empty review array.
-    return of([]);
-  }
-  return this.http.get<Review[]>(`${this.reviewsUrl}/?name=${term}`).pipe(
-    catchError(this.handleError<Review[]>('searchreviews', []))
-  );
-}
-
-  private handleError<T> (operation = 'operation', result?: T) {
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-   
+
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
-   
+
       // TODO: better job of transforming error for user consumption
       console.log(`${operation} failed: ${error.message}`);
-   
+
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
